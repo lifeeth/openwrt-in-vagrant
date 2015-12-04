7887194b4e657ffb2446a8610e5c998cfbd0aa2a
Praneeth Bodduluri <lifeeth@gmail.com>
2015-12-05 01:31:13 +0530

    Add script to autocreate the vagrant box - Add example Vagrant file that uses the box uploaded to atlas

---
+++ Vagrantfile
@@ -0,0 +1,30 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# All Vagrant configuration is done below. The "2" in Vagrant.configure
+# configures the configuration version (we support older styles for
+# backwards compatibility). Please don't change it unless you know what
+# you're doing.
+Vagrant.configure(2) do |config|
+  
+  config.vm.box = "lifeeth/openwrt-15.05-x86"
+
+  # Change SSH username and pass for vagrant
+  config.ssh.username = "root"
+  config.ssh.password = "root"
+  config.ssh.shell = "/bin/ash"
+  
+  # Create a forwarded port mapping which allows access to a specific port
+  # within the machine from a port on the host machine.
+  # LuCI is accessible from localhost:8080 in this case
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+
+  # Create a private network, which allows host-only access to the machine
+  # using a specific IP - This is taken over by openwrt as lan.
+  config.vm.network "private_network" , ip: "192.168.50.4",
+    auto_config: false
+
+  # We do not support vagrant synced folder yet.
+  config.vm.synced_folder ".", "/vagrant", disabled: true
+
+end
---
+++ utils/create_box.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+NAME="openwrt"
+URL="https://downloads.openwrt.org/chaos_calmer/15.05/x86/generic/openwrt-15.05-x86-generic-combined-ext4.img.gz"
+VDI="./openwrt.15.05.vdi"
+VMNAME="openwrt-15.05-x86"
+SIZE="104857600"
+OPENWRT_SRC=".openwrt_src"
+
+mkdir -p $OPENWRT_SRC
+
+if [ ! -f $OPENWRT_SRC/openwrt.img.gz ]
+then
+    curl $URL -o $OPENWRT_SRC/openwrt.img.gz
+fi
+
+# Inspired from http://hoverbear.org/2014/11/23/openwrt-in-virtualbox/
+cat $OPENWRT_SRC/openwrt.img.gz | gunzip | VBoxManage convertfromraw --format VDI stdin $VDI $SIZE
+VBoxManage createvm --name $VMNAME --register && \
+VBoxManage modifyvm $VMNAME \
+    --description "A VM to build an OpenWRT Vagrant box." \
+    --ostype "Linux26" \
+    --memory "512" \
+    --cpus "1" \
+    --nic1 "nat" \
+    --natdnshostresolver1 "on" \
+    --natpf1 "ssh,tcp,,2222,,22" \
+    --natpf1 "luci,tcp,,8080,,80" \
+    --nic2 "nat" \
+    --natdnshostresolver2 "on" \
+    --uart1 "0x3F8" "4" \
+    --uartmode1 server "`pwd`/serial" && \
+VBoxManage storagectl $VMNAME \
+    --name "SATA Controller" \
+    --add "sata" \
+    --portcount "4" \
+    --hostiocache "on" \
+    --bootable "on" && \
+VBoxManage storageattach $VMNAME \
+    --storagectl "SATA Controller" \
+    --port "1" \
+    --type "hdd" \
+    --nonrotational "on" \
+    --medium $VDI
+
+# Start the VM
+VBoxManage startvm $VMNAME --type "headless"
+
+# Use the serial port to make the image compatible with VirtualBox and comfortable for us.
+echo "Sleeping for 30s to let the image boot."
+sleep 30s
+echo "" |  nc -U serial
+echo "" |  nc -U serial
+
+echo "sed -i 's/eth0/eth2/g' /etc/config/network && sed -i 's/eth1/eth0/g' /etc/config/network && sed -i 's/eth2/eth1/g' /etc/config/network && \
+    uci set firewall.@zone[1].input='ACCEPT' && uci set firewall.@zone[1].forward='ACCEPT' && uci commit && echo "poweroff" >> /sbin/shutdown && \
+    chmod a+x /sbin/shutdown && echo -e 'root\nroot' | (passwd root) && \
+    opkg update && opkg install sudo && poweroff " | nc -U serial
+
+echo "Waiting for commands to execute."
+
+until $(VBoxManage showvminfo --machinereadable $VMNAME | grep -q ^VMState=.poweroff.)
+do
+      sleep 1
+done
+# Export as Vagrant Box and Delete the VM
+vagrant package --base $VMNAME && VBoxManage unregistervm $VMNAME --delete
